b0cb1d12d4409a49ddea1affe244f2e84921bbef
KarlisKalkis <kkalkis21@gmail.com>
2021-11-25 20:56:44 +0200

    spele

--- main.cpp
+++ main.cpp
@@ -1,34 +1,139 @@
-#include<iostream>
-#include<conio.h>
-#include<dos.h>
-#include <windows.h>
-#include <time.h> #define SCREEN_WIDTH 90 
-#define SCREEN_HEIGHT 30
-#define WIN_WIDTH 70
-#define MENU_WIDTH 20
-#define GAP_SIZE 7
-#define Enemy_DIF 45
-
+#include <iostream>
 using namespace std;
 
-HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-COORD CursorPosition;
+char kvadrati[10] = {'o','1','2','3','4','5','6','7','8','9'};
 
-int enemyY[3];
-int enemyX[3];
-int enemyFlag[3];
-char asteroid[3][4] = {' ', '+', '+', ' ',
-                        '|', '+', '+', '|',
-                        '_', '_', '_', '_'}
-int asteroidPos = WIN_WIDTH/2;
-int score = 0;
-int bullets[20][4];
-int bulletsLife[20];
-int bIndex = 0;
+int uzvaras_parbaude();
+void speles_laukums();
 
-void gotoxy(int x, int y){
-    CursorPosition.X = x;
-    CursorPosition.Y = y;
-    SetConsoleCursorPosition(console, CursorPosition);
+int main()
+{
+	int Speletajs = 1,i,izvele;
+
+    char mark;
+    do
+    {
+        speles_laukums();
+        Speletajs=(Speletajs%2)?1:2;
+
+        cout << "Speletajs " << Speletajs << ", ievadi speles laukuma zonu:  ";
+        cin >> izvele;
+
+        mark=(Speletajs == 1) ? 'X' : 'O';
+
+        if (izvele == 1 && kvadrati[1] == '1')
+
+            kvadrati[1] = mark;
+        else if (izvele == 2 && kvadrati[2] == '2')
+
+            kvadrati[2] = mark;
+        else if (izvele == 3 && kvadrati[3] == '3')
+
+            kvadrati[3] = mark;
+        else if (izvele == 4 && kvadrati[4] == '4')
+
+            kvadrati[4] = mark;
+        else if (izvele == 5 && kvadrati[5] == '5')
+
+            kvadrati[5] = mark;
+        else if (izvele == 6 && kvadrati[6] == '6')
+
+            kvadrati[6] = mark;
+        else if (izvele == 7 && kvadrati[7] == '7')
+
+            kvadrati[7] = mark;
+        else if (izvele == 8 && kvadrati[8] == '8')
+
+            kvadrati[8] = mark;
+        else if (izvele == 9 && kvadrati[9] == '9')
+
+            kvadrati[9] = mark;
+        else
+        {
+            cout<<"Nederīga izvēle ";
+
+            Speletajs--;
+            cin.ignore();
+            cin.get();
+        }
+        i=uzvaras_parbaude();
+
+        Speletajs++;
+    }while(i==-1);
+    speles_laukums();
+    if(i==1)
+
+        cout<<"==>\aSpeletajs "<<--Speletajs<<" uzvar!!!!!!!!!! ";
+    else
+        cout<<"==>\aSpēles rezultāts ir neizšķirts";
+
+    cin.ignore();
+    cin.get();
+    return 0;
 }
-void setcursor
+
+/**Funkcijas kas atgriež spēles rezultātu
+    1 Spēle ar uzvarētāju
+    -1 Spēle vēl notiek
+    O Spēle ir neizšķirta
+**********************************************/
+
+int uzvaras_parbaude()
+{
+    if (kvadrati[1] == kvadrati[2] && kvadrati[2] == kvadrati[3])
+
+        return 1;
+    else if (kvadrati[4] == kvadrati[5] && kvadrati[5] == kvadrati[6])
+
+        return 1;
+    else if (kvadrati[7] == kvadrati[8] && kvadrati[8] == kvadrati[9])
+
+        return 1;
+    else if (kvadrati[1] == kvadrati[4] && kvadrati[4] == kvadrati[7])
+
+        return 1;
+    else if (kvadrati[2] == kvadrati[5] && kvadrati[5] == kvadrati[8])
+
+        return 1;
+    else if (kvadrati[3] == kvadrati[6] && kvadrati[6] == kvadrati[9])
+
+        return 1;
+    else if (kvadrati[1] == kvadrati[5] && kvadrati[5] == kvadrati[9])
+
+        return 1;
+    else if (kvadrati[3] == kvadrati[5] && kvadrati[5] == kvadrati[7])
+
+        return 1;
+    else if (kvadrati[1] != '1' && kvadrati[2] != '2' && kvadrati[3] != '3' 
+                    && kvadrati[4] != '4' && kvadrati[5] != '5' && kvadrati[6] != '6' 
+                  && kvadrati[7] != '7' && kvadrati[8] != '8' && kvadrati[9] != '9')
+
+        return 0;
+    else
+        return -1;
+}
+
+void speles_laukums()
+{
+    system("cls");
+    cout << "\n\n\tKrustiņi un nullītes\n\n";
+
+    cout << "Speletajs 1 (X)  -  Speletajs 2 (O)" << endl << endl;
+    cout << endl;
+
+    cout << "     |     |     " << endl;
+    cout << "  " << kvadrati[1] << "  |  " << kvadrati[2] << "  |  " << kvadrati[3] << endl;
+
+    cout << "_____|_____|_____" << endl;
+    cout << "     |     |     " << endl;
+
+    cout << "  " << kvadrati[4] << "  |  " << kvadrati[5] << "  |  " << kvadrati[6] << endl;
+
+    cout << "_____|_____|_____" << endl;
+    cout << "     |     |     " << endl;
+
+    cout << "  " << kvadrati[7] << "  |  " << kvadrati[8] << "  |  " << kvadrati[9] << endl;
+
+    cout << "     |     |     " << endl << endl;
+}
+
